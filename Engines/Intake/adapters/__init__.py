dd974e54212914022e5e65be300b9a1c59e3027c
davidbedwell <david.bedwell07@gmail.com>
2026-09-07 15:59:05 -0700

    Export CSV typing error

--- Engines/Intake/adapters/__init__.py
+++ Engines/Intake/adapters/__init__.py
@@ -1,10 +1,11 @@
 from .base import SourceAdapter, SourcePayload
-from .csv_adapter import CSVAdapter
+from .csv_adapter import CSVAdapter, CSVTypeConversionError
 from .dataframe_adapter import DataFrameAdapter
 from .parquet_adapter import ParquetAdapter
 
 __all__ = [
     "CSVAdapter",
+    "CSVTypeConversionError",
     "DataFrameAdapter",
     "ParquetAdapter",
     "SourceAdapter",
